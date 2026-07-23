c8af83534e1b433f25a387c5cd4cd8651450fd5d
Ghirundai <matteo.haenen@gmail.com>
2026-07-23 14:53:11 +0300

    removed lxqt, added xfce

--- build_files/build.sh
+++ build_files/build.sh
@@ -37,7 +37,7 @@ dnf -y install wofi
 # gtk customization tool
 dnf -y install nwg-look
 #lxqt desktop manager
-dnf -y install lxqt-desktop
+dnf -y install @xfce-desktop
 
 # DESKTOP APPS
 # terminal
