095a3a9074da494f9beb519defdccc686d791926
Ghirundai <64478434+Ghirundai@users.noreply.github.com>
2026-09-21 21:16:10 +0300

    testing official heroic copr + ghostty copr

--- build_files/build.sh
+++ build_files/build.sh
@@ -43,7 +43,8 @@ dnf -y install @xfce-desktop
 
 # DESKTOP APPS
 # terminal
-#dnf -y install ghostty-nightly
+dnf -y copr enable ponesicek/ghostty-bin
+dnf -y install ghostty
 # file manager
 dnf -y install pcmanfm
 # ark for opening archives
@@ -104,6 +105,7 @@ dnf -y install ffmpeg x264-libs obs-studio obs-studio-plugin-x264 --allowerasing
 
 # GAMING
 # packages copied from rakuos setup-gaming
+dnf -y enable copr atim/heroic-games-launcher
 dnf -y install steam lutris heroic-games-launcher lact goverlay mangohud mangohud.i686 protonplus protontricks vkBasalt vkBasalt.i686
 # Prismlauncher for Minecraft
 dnf -y install prismlauncher
